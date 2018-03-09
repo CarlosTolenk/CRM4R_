d2ef12f1d574189a2d3cd4cf9962b49d5a6bd6ec
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2018-03-08 22:03:25 -0400

    Estadistas actualizadas

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -10,6 +10,12 @@ exports.getCount = (req, res) => {
 
   let totales = new Object;
   let prestamo = new Prestamo();
+  let monto_original = 0;
+  let monto_total = 0;
+  let cliente_activo = 0;
+  let cliente_id = '';
+
+
 
     Cliente.count({}, (err, countC) => {
       if(err) return res.status(500).send({message: 'Error en la peteción'});
@@ -28,15 +34,36 @@ exports.getCount = (req, res) => {
       if(err) return res.status(500).send({message: 'Error en la peteción'});
       if(!countT) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
       totales.ticket = countT;
+      //return res.status(200).send({Estadisticas: totales});
+    });
+
+    Prestamo.find().exec((err, prestamos) => {
+      if(err) return res.status(500).send({message: 'Error en la peteción'});
+      if(!prestamos) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+
+      //un ciclo para recoger toda la base de datos e ir sumando los montos de los prestamos
+      prestamos.forEach((prestamos) => {
+        monto_original += prestamos.monto_original;
+        monto_total += prestamos.monto_total;
+      });
+      console.log(cliente_activo);
+      totales.monto_prestamo = monto_original;
+      totales.monto_esperado = monto_total;
+
       return res.status(200).send({Estadisticas: totales});
     });
 
 
 
-    /* Model.find({}, 'first last', function (err, docs) {
-  // docs is an array of partially-`init`d documents
-  // defaults are still applied and will be "populated"
-}) */
+
+
+/*
+teams.forEach((teams) => {
+  if(teams && teams._id != teamId) team_isset = true;
+});
+
+
+*/
 
 
 
