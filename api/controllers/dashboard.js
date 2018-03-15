e63e3370bef4adfe742176d995faddaddd08cd9f
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2018-03-15 15:14:14 -0400

    Mejorado el metodo de estadisticas

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -8,73 +8,158 @@ const Ticket = require('../models/tickets');
 
 exports.getCount = (req, res) => {
 
-  let totales = new Object;
-  let prestamo = new Prestamo();
-  let monto_original = 0;
-  let monto_total = 0;
-  let cliente_activo = 0;
-  let ticket_completados = 0;
+    getCounter().then((response) => {
+      return res.status(200).send(response);
+    });
+};
 
-    Cliente.count({}, (err, countC) => {
+
+
+
+
+async function getCounter(){
+
+  let totales = new Object;
+  let Prestamos;
+  let Clientes;
+  let Tickets;
+
+
+  let TotalCliente = await Cliente.count({}, (err, countC) => {
       if(err) return res.status(500).send({message: 'Error en la peteción'});
       if(!countC) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
       totales.clientes = countC;
     });
 
-    Prestamo.count({}, (err, countP) => {
-      if(err) return res.status(500).send({message: 'Error en la peteción'});
-      if(!countP) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
-      totales.prestamos = countP;
-    //  return res.status(200).send({Estadisticas: totales});
+    let TotalPrestamo = await Prestamo.count({}, (err, countP) => {
+          if(err) return res.status(500).send({message: 'Error en la peteción'});
+          if(!countP) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+          totales.prestamos = countP;
     });
 
-    Ticket.count({}, (err, countT) => {
-      if(err) return res.status(500).send({message: 'Error en la peteción'});
-      if(!countT) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
-      totales.ticket = countT;
-      //return res.status(200).send({Estadisticas: totales});
-    });
-
-    Prestamo.find().exec((err, prestamos) => {
-      if(err) return res.status(500).send({message: 'Error en la peteción'});
-      if(!prestamos) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
-
-      //un ciclo para recoger toda la base de datos e ir sumando los montos de los prestamos
-      prestamos.forEach((prestamos) => {
-        monto_original += prestamos.monto_original;
-        monto_total += prestamos.monto_total;
+    let TotalTicket =  await Ticket.count({}, (err, countT) => {
+          if(err) return res.status(500).send({message: 'Error en la peteción'});
+          if(!countT) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+          totales.ticket = countT;
       });
-      totales.monto_prestamo = monto_original;
-      totales.monto_esperado = monto_total;
 
-      // return res.status(200).send({Estadisticas: totales});
+     await CalculosPrestamo().then(response =>{
+         Prestamos = response;
+       });
+
+     await ClientesActivos(totales.clientes).then(response =>{
+         Clientes = response;
+       });
+
+     await TicketsCompletados(totales.ticket).then(response =>{
+         Tickets = response;
+       });
+
+
+      //     await Cliente.find().exec((err, clientes) => {
+      //     if(err) return res.status(500).send({message: 'Error en la peteción'});
+      //     if(!clientes) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+      //
+      //     clientes.forEach((clientes) => {
+      //       if(clientes.activo == 'true') cliente_activo++;
+      //     });
+      //
+      //     totales.clientes_activos = cliente_activo;
+      //     return cliente_activo;
+      //
+      //   });
+      //
+      //  await Ticket.find().exec((err, ticket) => {
+      //       if(err) return res.status(500).send({message: 'Error en la peteción'});
+      //       if(!ticket) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+      //
+      //       ticket.forEach((ticket) => {
+      //         if(ticket.estado == "COMPLETADO") ticket_completados++;
+      //       });
+      //
+      //       totales.ticket_completados = ticket_completados;
+      //
+      // });
+
+
+
+
+
+    return {TotalTicket, TotalPrestamo, TotalCliente, Clientes, Prestamos, Tickets };
+}
+
+
+
+
+// Crear una funcion para cada uno de los metodos
+
+async function CalculosPrestamo(){
+  let infoPrestamo = new Object;
+  let monto_original = 0;
+  let monto_total = 0;
+
+    return new Promise(resolve => {
+      Prestamo.find().exec((err, prestamos) => {
+          if(err) return res.status(500).send({message: 'Error en la peteción'});
+          if(!prestamos) return res.status(404).send({message: 'No se ha podido calcular'});
+
+          //un ciclo para recoger toda la base de datos e ir sumando los montos de los prestamos
+            prestamos.forEach((prestamos) => {
+            monto_original += prestamos.monto_original;
+            monto_total += prestamos.monto_total;
+          });
+          infoPrestamo.monto_original = monto_original;
+          infoPrestamo.monto_total = monto_total
+          resolve(infoPrestamo);
+      });
     });
+  }
 
-    Cliente.find().exec((err, clientes) => {
+  async function ClientesActivos(totalCliente){
+
+    let infoClientes = new Object;
+    let clientes_activos = 0;
+
+    return new Promise(resolve => {
+
+       Cliente.find().exec((err, clientes) => {
       if(err) return res.status(500).send({message: 'Error en la peteción'});
       if(!clientes) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
 
       clientes.forEach((clientes) => {
-        if(clientes.activo == 'true') cliente_activo++;
+        if(clientes.activo == 'true') clientes_activos++;
       });
 
-      totales.clientes_activos = cliente_activo;
+        infoClientes.clientes_activos = clientes_activos;
+        infoClientes.clientes_candidatos = totalCliente - clientes_activos;
+        resolve(infoClientes);
+      });
 
-         // return res.status(200).send({Estadisticas: totales});
     });
 
-    Ticket.find().exec((err, ticket) => {
-      if(err) return res.status(500).send({message: 'Error en la peteción'});
-      if(!ticket) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
+  }
+
+async function TicketsCompletados(TotalTickets){
+
+  let infoTickets = new Object;
+  let ticket_completados = 0;
+
+  return new Promise(resolve => {
+
+      Ticket.find().exec((err, ticket) => {
+        if(err) return res.status(500).send({message: 'Error en la peteción'});
+        if(!ticket) return res.status(404).send({message: 'No se ha podido actualizar el cliente'});
 
       ticket.forEach((ticket) => {
         if(ticket.estado == "COMPLETADO") ticket_completados++;
       });
 
-      totales.ticket_completados = ticket_completados;
-      console.log(totales);
-      return res.status(200).send({Estadisticas: totales});
+      infoTickets.ticket_completados = ticket_completados;
+      infoTickets.ticket_proceso = TotalTickets - ticket_completados;
+      resolve(infoTickets);
+
     });
 
+  });
 
-};
+}
